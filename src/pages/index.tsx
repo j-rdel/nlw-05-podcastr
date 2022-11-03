7fbd985bbc15879a2d93e3fd4f7ed49303af669f
Jardel Urban <jardelurban3@gmail.com>
2022-11-03 19:38:28 -0300

    ajustes de versionamento

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -114,13 +114,55 @@ export default function Home({latestEpisodes, allEpisodes}: HomeProps) {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const {data} = await api.get('episodes' , {
-    params: {
-      _limit: 12,
-      _sort: 'published_at',
-      _order: 'desc'
-    }
-  })
+  // const {data} = await api.get('episodes' , {
+  //   params: {
+  //     _limit: 12,
+  //     _sort: 'published_at',
+  //     _order: 'desc'
+  //   }
+  // })
+
+  const data = [
+      {
+          "id": "a-importancia-da-contribuicao-em-open-source",      
+          "title": "Faladev #30 | A importância da contribuição em Open Source",
+          "members": "Diego Fernandes, João Pedro, Diego Haz e Bruno Lemos",
+          "published_at": "2021-01-22 16:35:40",
+          "thumbnail": "https://storage.googleapis.com/golden-wind/nextlevelweek/05-podcastr/opensource.jpg",
+          "description": "<p>Nesse episódio do Faladev, Diego Fernandes se reúne com João Pedro Schmitz, Bruno Lemos e Diego Haz, para discutir sobre a importância da contribuição open source e quais desafios circulam na comunidade.</p><p>A gente passa a maior parte do tempo escrevendo código. Agora chegou o momento de falar sobre isso. Toda semana reunimos profissionais da tecnologia para discutir sobre tudo que circula na órbita da programação.</p><p>O Faladev é um podcast original Rocketseat.</p>",
+          "file": {
+            "url": "https://storage.googleapis.com/golden-wind/nextlevelweek/05-podcastr/audios/opensource.m4a",
+            "type": "audio/x-m4a",
+            "duration": 3981
+          }
+        },
+        {
+          "id": "a-importancia-da-contribuicao-em-open-source-2",      
+          "title": "Faladev #30 | A importância da contribuição em Open Source",
+          "members": "Diego Fernandes, João Pedro, Diego Haz e Bruno Lemos",
+          "published_at": "2021-01-22 16:35:40",
+          "thumbnail": "https://storage.googleapis.com/golden-wind/nextlevelweek/05-podcastr/opensource.jpg",
+          "description": "<p>Nesse episódio do Faladev, Diego Fernandes se reúne com João Pedro Schmitz, Bruno Lemos e Diego Haz, para discutir sobre a importância da contribuição open source e quais desafios circulam na comunidade.</p><p>A gente passa a maior parte do tempo escrevendo código. Agora chegou o momento de falar sobre isso. Toda semana reunimos profissionais da tecnologia para discutir sobre tudo que circula na órbita da programação.</p><p>O Faladev é um podcast original Rocketseat.</p>",
+          "file": {
+            "url": "https://storage.googleapis.com/golden-wind/nextlevelweek/05-podcastr/audios/opensource.m4a",
+            "type": "audio/x-m4a",
+            "duration": 3981
+          }
+        },
+        {
+          "id": "a-importancia-da-contribuicao-em-open-source-2",      
+          "title": "Faladev #30 | A importância da contribuição em Open Source",
+          "members": "Diego Fernandes, João Pedro, Diego Haz e Bruno Lemos",
+          "published_at": "2021-01-22 16:35:40",
+          "thumbnail": "https://storage.googleapis.com/golden-wind/nextlevelweek/05-podcastr/opensource.jpg",
+          "description": "<p>Nesse episódio do Faladev, Diego Fernandes se reúne com João Pedro Schmitz, Bruno Lemos e Diego Haz, para discutir sobre a importância da contribuição open source e quais desafios circulam na comunidade.</p><p>A gente passa a maior parte do tempo escrevendo código. Agora chegou o momento de falar sobre isso. Toda semana reunimos profissionais da tecnologia para discutir sobre tudo que circula na órbita da programação.</p><p>O Faladev é um podcast original Rocketseat.</p>",
+          "file": {
+            "url": "https://storage.googleapis.com/golden-wind/nextlevelweek/05-podcastr/audios/opensource.m4a",
+            "type": "audio/x-m4a",
+            "duration": 3981
+          }
+        },
+      ]
 
   const episodes = data.map(episode =>{
     return {
